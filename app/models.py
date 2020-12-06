c652bd60fa7b5c3e97955cba4381e054bb9ffc3d
Preston-Too <toopreston@gmail.com>
2020-12-06 17:22:58 +0300

    added class pitch to models.py

--- app/models.py
+++ app/models.py
@@ -35,3 +35,34 @@ class User(UserMixin,db.Model):
         
     def __repr__(self):
         return f'User {self.username}'
+
+
+class Pitch(db.Model):
+
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    pitch_id = db.Column(db.Integer)
+    pitch_title = db.Column(db.String)
+    pitch_category = db.Column(db.String)
+    pitch_comment = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    likes = db.Column(db.Integer)
+    dislikes = db.Column(db.Integer)
+
+    comments = db.relationship('Comment',backref =  'pitch',lazy = "dynamic")
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitches(cls,category):
+        pitches = Pitch.query.filter_by(pitch_category=category).all()
+        return pitches
+
+    @classmethod
+    def getPitchId(cls,id):
+        pitch = Pitch.query.filter_by(id=id).first()
+        return pitch
